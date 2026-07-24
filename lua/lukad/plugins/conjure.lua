e5dbf2b741d63ed3bfb33dacd69df21a70d36973
Luka Dornhecker <luka@fnord.tech>
2026-07-24 10:58:07 +0200

    fix conjure crapping itself in rust repos when evcxr is not installed

--- lua/lukad/plugins/conjure.lua
+++ lua/lukad/plugins/conjure.lua
@@ -1,3 +1,13 @@
 return {
 	"Olical/conjure",
+	config = function()
+		local rust = require("conjure.client.rust.evcxr")
+		local auto_start = rust["on-load"]
+
+		rust["on-load"] = function()
+			if vim.fn.executable("evcxr") == 1 then
+				return auto_start()
+			end
+		end
+	end,
 }
