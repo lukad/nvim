43025b3713fd262e7d3b1bda71e1ae5e37368342
Luka Dornhecker <luka@fnord.tech>
2026-07-24 21:50:33 +0200

    update conjure

--- lua/lukad/plugins/conjure.lua
+++ lua/lukad/plugins/conjure.lua
@@ -1,14 +1,6 @@
 return {
   "Olical/conjure",
-  config = function()
-    local rust = require("conjure.client.rust.evcxr")
-    local auto_start = rust["on-load"]
-    rust["on-load"] = nil
-
-    rust["on-load"] = function()
-      if vim.fn.executable("evcxr") == 1 then
-        return auto_start()
-      end
-    end
-  end,
+  -- init = function()
+  --   vim.g["conjure#stdio#silence_missing_command"] = true
+  -- end,
 }
